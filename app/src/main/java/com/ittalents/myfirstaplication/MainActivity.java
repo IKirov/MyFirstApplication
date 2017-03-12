57146fa9373f1dcefaf1eb87541a88865d1e9df2
Mariq <atanasova19@abv.bg>
2017-03-12 10:27:57 +0200

    create application

---
+++ app/src/main/java/com/ittalents/myfirstaplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.ittalents.myfirstaplication;
+
+import android.graphics.Color;
+import android.support.annotation.ColorRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+}
